d24e47c89c0ba4f7ad34cd8d18e8ed2c1ba810ff
Djordje Kojic <64-2022@pmf.kg.ac.rs>
2026-08-20 16:16:36 +0200

    uradjena prva verzija dashboarda sa statickim podacima

--- frontend/dashboard/src/app/app.ts
+++ frontend/dashboard/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, AfterViewInit,signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,101 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements AfterViewInit {
   protected readonly title = signal('dashboard');
+
+  ngAfterViewInit(): void {
+    this.createChart1();
+    this.createChart2();
+    this.createChart3();
+  }
+  createChart1(): void {
+    new Chart('chart1', {
+      type: 'bar',
+
+      data: {
+        labels: [
+          'A',
+          'B',
+          'C',
+          'D',
+          'E',
+          'F',
+          'G',
+          'H',
+          'I',
+          'J',
+          'K',
+          'L'
+        ],
+
+        datasets: [{
+          label: 'Temperatura',
+          data: [12, 25, 18, 32, 20, 27,12, 25, 18, 32, 20, 27]
+        }]
+      },
+
+      options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    });
+  }
+
+
+  createChart2(): void {
+    new Chart('chart2', {
+      type: 'bar',
+
+      data: {
+        labels: [
+          'A',
+          'B',
+          'C',
+          'D',
+          'E',
+          'F'
+        ],
+
+        datasets: [{
+          label: 'Vlaznost',
+          data: [15, 22, 17, 28, 19, 25]
+        }]
+      },
+      
+
+      options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    });
+  }
+  createChart3(): void {
+    new Chart('chart3', {
+      type: 'line',
+
+      data: {
+        labels: [
+          'A',
+          'B',
+          'C',
+          'D',
+          'E',
+          'F'
+        ],
+
+        datasets: [{
+          label: 'Osvetljenje',
+          data: [20, 35, 28, 42, 31, 45],
+          fill: true,
+          tension: 0.4
+        }]
+      },
+
+      options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    });
+  }
 }
